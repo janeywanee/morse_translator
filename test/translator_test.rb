f847928c51b4299c6c7bc915dcd461abd61a9fd3
Jane Kim <Janeekim12@gmail.com>
2018-01-31 12:41:01 -0700

    new test added

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,19 @@
-# YOUR TESTS GO IN HERE!
+require "minitest/autorun"
+require "./lib/translator"
+
+class TranslatorTest <Minitest::Test
+
+def test_one_lowercase_letters
+  translate = Translator.new
+
+  assert_instance_of Translator, translate
+end
+
+
+
+
+
+
+
+
+end# YOUR TESTS GO IN HERE!
